4a4dca4ca29f250a018f07d7de5196ab43ab2547
sachin.kunte@siemens.com <sachin.kunte@siemens.com>
2024-10-06 13:24:44 +0530

    changed the message which is shown

--- HelloWorldConsoleApplication/HelloWorldConsoleApplication/Program.cs
+++ HelloWorldConsoleApplication/HelloWorldConsoleApplication/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, all , how are you!");
         }
     }
 }
